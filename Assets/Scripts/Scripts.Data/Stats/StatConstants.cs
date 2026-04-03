ac8626e8dbab3c3ec5b9222f4a314abe15566e6e
IAFahim <ishtiaq.fahim@northsouth.edu>
2026-04-03 17:59:00 +0600

    chore: update project settings and timeline branch state

--- Assets/Scripts/Scripts.Data/Stats/StatConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace Scripts.Data.Stats
-{
-    public static class StatConstants
-    {
-        public const ushort NoLink = ushort.MaxValue;
-        public const int MaxStatCount = ushort.MaxValue;
-    }
-}
