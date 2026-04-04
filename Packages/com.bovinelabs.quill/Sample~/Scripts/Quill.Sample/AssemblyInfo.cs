cfb0805bda3c5fa60923559b1ee9f8f83e9f0e0f
IAFahim <ishtiaq.fahim@northsouth.edu>
2026-04-04 15:04:29 +0600

    removed stupid sample

--- Packages/com.bovinelabs.quill/Sample~/Scripts/Quill.Sample/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// <copyright file="AssemblyInfo.cs" company="DefaultCompany">
-// Copyright (c) DefaultCompany. All rights reserved.
-// </copyright>
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Quill.Sample.Debug")]
-[assembly: InternalsVisibleTo("Quill.Sample.Editor")]
-[assembly: InternalsVisibleTo("Quill.Sample.Tests")]
