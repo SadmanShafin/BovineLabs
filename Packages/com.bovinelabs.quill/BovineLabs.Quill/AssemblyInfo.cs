b664b755d48172329e5f0036f16a399ac98560f0
IAFahim <ishtiaq.fahim@northsouth.edu>
2026-04-07 20:16:01 +0600

    remove old

--- Packages/com.bovinelabs.quill/BovineLabs.Quill/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// <copyright file="AssemblyInfo.cs" company="BovineLabs">
-//     Copyright (c) BovineLabs. All rights reserved.
-// </copyright>
-
-using System.Runtime.CompilerServices;
-using Unity.Entities;
-
-[assembly: InternalsVisibleTo("BovineLabs.Quill.Debug")]
-[assembly: RegisterUnityEngineComponentType(typeof(UnityEngine.Camera))]
