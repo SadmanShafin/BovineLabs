1285643a486a40ad04966f8422fb3ac626664c3e
IAFahim <iafahim.dev@gmail.com>
2026-05-27 23:00:01 +0600

    chore: auto-update all packages [2026-05-27 23:00:01]

--- Packages/BovineLabs.Timeline.Distance/BovineLabs.Timeline.Distance.Tests/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// <copyright file="AssemblyInfo.cs" company="iafahim">
-// Copyright (c) iafahim. All rights reserved.
-// </copyright>
-
-using Unity.Entities;
-
-[assembly: DisableAutoCreation]
--- Packages/BovineLabs.Timeline.Distance/BovineLabs.Timeline.Distance/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("BovineLabs.Timeline.Distance.Debug")]
-[assembly: InternalsVisibleTo("BovineLabs.Timeline.Distance.Editor")]
-[assembly: InternalsVisibleTo("BovineLabs.Timeline.Distance.Tests")]
